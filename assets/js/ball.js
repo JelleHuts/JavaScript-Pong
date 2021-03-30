64a5a8ccb832b5afe5c73a862fc52a3f19e9b72f
u000000 <200516@student.glu.nl>
2021-03-30 13:16:40 +0200

    Stap 3

--- assets/js/ball.js
+++ assets/js/ball.js
@@ -1,19 +1,14 @@
-// class Ball {
-//     constructor() {
-//         this.color = color;
-//         this.x = x;
-//         this.y = y;
-//         this.width = 10;
-//         this.height = 10;
-//     }
+class Ball extends Rectangle{
+    constructor(x=0, y=0, color='white') {
+        super(x, y, 24, 24);
+        this.color = color;
+        this.speed = 0;
+        this.startX = x;
+        this.startY = y;
+        this.angle = 0;
+        this.locked = false;
+        this.out = false;
+    }
 
-//     update() {
+}
 
-//     }
-
-//     draw(){
-
-//     }
-// }
-
-// const b = new Ball('orange', 100, 25);
--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,11 @@
 class Game {
     constructor() {
+        // Verbinden met het pong canvas.
+        this.canvas = document.getElementById('pong');
+        this.context = this.canvas.getContext('2d');
+
+        // ball object aanmaken
+        this.ball = new Ball(this.canvas.width/2, this.canvas.height/2, 'orange');
 
         // Gameloop
         let lastTime;
@@ -15,9 +21,16 @@ class Game {
     }
 
     update(deltatime) {
-        
-    }
-    draw() {
 
+    }
+
+    draw() {
+        this.drawRectangle(this.context, this.ball)
+
+    }
+
+    drawRectangle(ctx, rect, color='white') {
+        ctx.fillStyle = color;
+        ctx.fillRect(rect.left, rect.top, rect.size.x, rect.size.y);
     }
 }
